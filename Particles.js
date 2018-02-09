7a0c0ca88fdb8b07ef577d79319e16f0d28d49ba
galactustrombone <dolllyamar@gmail.com>
2018-02-09 18:46:45 -0500

    maybe performance improvement for particles

--- Particles.js
+++ Particles.js
@@ -22,7 +22,7 @@ function update() {
 	for (let i = 0; i < n - 1; i += 1) {
 		// if (ice[i][2] == 1) { // doesn't work for some reason, need to check if either is stuck rather than just ith
 		for (let j = i + 1; j < n; j += 1) {
-			if (ice[i][2] == 1 || ice[j][2] == 1) { // if either dot is stuck
+			if (ice[i][2] == 1 ^ ice[j][2] == 1) { // if either dot is stuck
 				if (ice[i][0] <= ice[j][0] + dim && ice[i][0] + dim >= ice[j][0] && ice[i][1] <= ice[j][1] + dim && ice[i][1] + dim >= ice[j][1]) { // and if they collide
 					ice[i][2] = ice[j][2] = 1; // they are both stuck
 				}
