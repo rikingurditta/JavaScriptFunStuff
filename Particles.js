1fe81c6cdf08c7b4841656cbd6325c7a71fcd44b
galactustrombone <dolllyamar@gmail.com>
2018-02-08 22:10:57 -0500

    small changes I think

--- Particles.js
+++ Particles.js
@@ -18,7 +18,7 @@ for (let i = 1; i < n; i += 1) {
 function update(dt) {
 	for (let i = 0; i < n - 1; i += 1) {
 		for (let j = i + 1; j < n; j += 1) {
-			if (ice[i][0] >= ice[j][0] && ice[i][0] <= (ice[j][0] + 4) && ice[i][1] >= ice[j][1] && ice[i][1] <= (ice[j][1] + 4) && (ice[i][2] == 1 || ice[j][2] == 1)) {
+			if (ice[i][0] >= ice[j][0] - 4 && ice[i][0] <= (ice[j][0] + 4) && ice[i][1] >= ice[j][1] - 4 && ice[i][1] <= (ice[j][1] + 4) && (ice[i][2] == 1 || ice[j][2] == 1)) {
 				ice[i][2] = ice[j][2] = 1;
 			}
 		}
