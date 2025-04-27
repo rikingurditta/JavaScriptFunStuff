253a06d2754fc1d2bd8b4f36e6a715d8cfe4be95
Rikin Gurditta <dolllyamar@gmail.com>
2025-04-27 09:11:49 -0400

    Massively sped up particles

--- particles/particles.js
+++ particles/particles.js
@@ -16,50 +16,72 @@ class Ice {
 	}
 }
 
+class BVH {
+	constructor(x, y, width, height) {
+
+	}
+}
+
 var allIceSet = new Set([]); // array of dots
 
-var n = 4096; // number of dots
+var n = 4096 * 2; // number of dots
 var dim = 2; // height/width of dot (square)
 
-var firstStuck = new Ice(w / 2, h / 2, dim, dim);
-var stuck = [firstStuck];
-var toRemove = new Set([]);
+var movingIce = new Array(n-1);
+var movingNum = n-1;
 
-for (let i = 0; i < n; i += 1) {
+var stuckIce = new Array(n);
+stuckIce[0] = new Ice(w / 2, h / 2, dim, dim);
+var stuckNum = 1;
+
+for (let i = 0; i < n - 1; i += 1) {
 	// randomly decide the rest
-	allIceSet.add(new Ice(Math.floor(Math.random() * w * 1 / 2) + w / 4,
-		Math.floor(Math.random() * h * 1 / 2) + h / 4), dim, dim);
+	movingIce[i] = new Ice(Math.floor(Math.random() * w * 1 / 2) + w / 4,
+		Math.floor(Math.random() * h * 1 / 2) + h / 4, dim, dim);
 }
 
 
 function update() {
-	stuck.forEach(function(stuckIce) {
-		allIceSet.forEach(function(nonStuckIce) {
-			if (stuckIce.x <= nonStuckIce.x + dim && stuckIce.x + dim >= nonStuckIce.x && stuckIce.y <= nonStuckIce.y + dim && stuckIce.y + dim >= nonStuckIce.y) { // and if they collide
-				stuck.push(nonStuckIce);
-				toRemove.add(nonStuckIce);
+	for (let i = 0; i < stuckNum; i++) {
+		stuck = stuckIce[i];
+		let j = 0;
+		while (j < movingNum) {
+			nonStuck = movingIce[j];
+			if (stuck.x <= nonStuck.x + dim && stuck.x + dim >= nonStuck.x && stuck.y <= nonStuck.y + dim && stuck.y + dim >= nonStuck.y) { // and if they collide
+				stuckIce[stuckNum] = nonStuck;
+				stuckNum++;
+				movingIce[j] = movingIce[movingNum - 1];
+				movingNum--;
+			} else {
+				j++;
 			}
-		})
-	})
-	toRemove.forEach((iceToRemove) => allIceSet.delete(iceToRemove));
+		}
+	}
+	for (let i = 0; i < movingNum; i++) {
+		movingIce[i].x += Math.floor(Math.random() * 3 - 1); // move it randomly
+		movingIce[i].y += Math.floor(Math.random() * 3 - 1);
+	}
 
-	allIceSet.forEach(function(nonStuckIce) {
-		nonStuckIce.x += Math.floor(Math.random() * 3 - 1); // move it randomly
-		nonStuckIce.y += Math.floor(Math.random() * 3 - 1);
-	});
+	// make stuck group move
+	// let randX = Math.floor(Math.random() * 3 - 1);
+	// let randY = Math.floor(Math.random() * 3 - 1);
+	// for (let i = 0; i < stuckNum; i++) {
+	// 	stuckIce[i].x += randX;
+	// 	stuckIce[i].y += randY;
+	// }
 }
 
 
 function render () {
 	context.clearRect(0, 0, w, h); // reset the canvas each time
 	context.fillStyle = 'black';
-	allIceSet.forEach(function(nonStuckIce) {
-		context.fillRect(nonStuckIce.x, nonStuckIce.y, dim, dim);
-	});
+	for (let i = 0; i < movingNum; i++) {
+		context.fillRect(movingIce[i].x, movingIce[i].y, dim, dim);
+	};
 	context.fillStyle = 'red';
-	stuck.forEach(function(stuckIce) {
-		context.fillRect(stuckIce.x, stuckIce.y, dim, dim);
-	});
+	for (let i = 0; i < stuckNum; i++) {
+		context.fillRect(stuckIce[i].x, stuckIce[i].y, dim, dim);
+	};
 }
 
 
