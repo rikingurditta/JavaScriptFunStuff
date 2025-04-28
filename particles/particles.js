0dd08b682223ad2adac2f38b016a7beaf533d5b5
Rikin Gurditta <dolllyamar@gmail.com>
2025-04-28 03:55:51 -0400

    Added quadtree to particles.js

--- particles/particles.js
+++ particles/particles.js
@@ -3,9 +3,14 @@
 // set up the canvas
 var canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
-var w = canvas.width = 768;
-var h = canvas.height = 768;
+var w = canvas.width = 1024;
+var h = canvas.height = 1024;
 document.body.appendChild(canvas);
+
+var n = 4096; // number of dots
+var dim = 3; // height/width of dot (square)
+
+var qt_size = 1;
 
 class Ice {
 	constructor(x, y, width, height) {
@@ -13,19 +18,163 @@ class Ice {
 		this.y = y;
 		this.width = dim;
 		this.height = dim;
+		this.stuck = false;
 	}
 }
 
-class BVH {
-	constructor(x, y, width, height) {
+class Quadtree {
+	constructor(x, y, width, height, max_items) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.subs = []
+		this.item_x = [];
+		this.item_y = [];
+		this.items = [];
+		this.num_items = 0;
+		this.max_items = max_items;
+		this.is_leaf = true;
+	}
 
+	in_range(x, y) {
+		if (x >= this.x && y >= this.y && x < this.x + this.width && y < this.y + this.height) {
+			return true;
+		}
+		return false;
+	}
+
+	add(x, y, item) {
+		let sub_w = this.width / 2;
+		let sub_h = this.height / 2;
+		if (!this.in_range(x, y)) {
+			let str = "out of range: item (" + x + ", " + y + ") "
+				    + "not in range of quadtree [" + this.x + ", " + (this.x + this.width)
+				    + "] x [" + this.y + ", " + (this.y + this.height) + "]";
+			throw new Error(str);
+		} else if (this.is_leaf && this.num_items < this.max_items) {
+			this.items.push(item);
+			this.item_x.push(x);
+			this.item_y.push(y);
+			this.num_items++;
+		} else if (this.is_leaf) {
+			this.subs.push(new Quadtree(this.x,         this.y,         sub_w, sub_h, this.max_items));
+			this.subs.push(new Quadtree(this.x + sub_w, this.y,         sub_w, sub_h, this.max_items));
+			this.subs.push(new Quadtree(this.x,         this.y + sub_h, sub_w, sub_h, this.max_items));
+			this.subs.push(new Quadtree(this.x + sub_w, this.y + sub_h, sub_w, sub_h, this.max_items));
+			this.num_items = 0;
+			this.is_leaf = false;
+			for (let i = 0; i < this.num_items; i++) {
+				this.add(this.item_x[i], this.item_y[i], this.items[i]);
+			}
+			this.add(x, y, item);
+		} else {
+			let sub_index = 0;
+			if (x >= this.x + sub_w) {
+				sub_index += 1;
+			}
+			if (y >= this.y + sub_h) {
+				sub_index += 2;
+			}
+			this.subs[sub_index].add(x, y, item);
+		}
+	}
+
+	print(depth = 0) {
+		let str = ""
+		for (let i = 0; i < depth; i++) {
+			str += "\t";
+		}
+		if (this.is_leaf) {
+			str += "quadtree [" + this.x + ", " + (this.x + this.width)
+				   + "] x [" + this.y + ", " + (this.y + this.height) + "]";
+
+			if (this.num_items > 0) {
+				str += " with items:"
+				for (let item of this.items) {
+					str += " " + item;
+				}
+			}
+		} else {
+			str += "quadtree [" + this.x + ", " + (this.x + this.width)
+				   + "] x [" + this.y + ", " + (this.y + this.height) + "] with subtrees:\n";
+			str += this.subs[0].print(depth+1) + "\n";
+			str += this.subs[1].print(depth+1) + "\n";
+			str += this.subs[2].print(depth+1) + "\n";
+			str += this.subs[3].print(depth+1)
+		}
+		return str;
+	}
+
+	get_items() {
+		if (this.is_leaf) {
+			return this.items;
+		} else {
+			let out = [];
+			for (let sub of this.subs) {
+				out.push(...sub.get_items());
+			};
+			return out;
+		}
+	}
+
+	get_neighbourhood(x, y) {
+		if (this.is_leaf) {
+			return this.items;
+		}
+		let out = [];
+		let bools = [true, true, true, true];
+		if (x < this.x + 0.25 * this.width) {
+			bools[1] = false;
+			bools[3] = false;
+		} else if (x >= this.x + 0.75 * this.width) {
+			bools[0] = false;
+			bools[2] = false;
+		}
+		if (y < this.y + 0.25 * this.height) {
+			bools[2] = false;
+			bools[3] = false;
+		} else if (y >= this.y + 0.75 * this.height) {
+			bools[0] = false;
+			bools[1] = false;
+		}
+		for (let i = 0; i < 4; i++) {
+			if (bools[i]) {
+				out.push(...this.subs[i].get_neighbourhood(x, y));
+			}
+		}
+		return out;
+	}
+
+	draw() {
+		context.rect(this.x, this.y, this.width, this.height);
+		for (let sub of this.subs) {
+			sub.draw();
+		};
 	}
 }
 
-var allIceSet = new Set([]); // array of dots
+q = new Quadtree(0, 0, 1, 1, 2);
+q.add(0.1, 0.1, "a");
+console.log("a")
+q.add(0.2, 0.2, "b");
+console.log("b")
+q.add(0.2, 0.1, "c");
+console.log("c")
+q.add(0.1, 0.2, "d");
+console.log("d")
+q.add(0.6, 0.2, "e");
+console.log("e")
+q.add(0.6, 0.1, "f");
+console.log("f")
+q.add(0.7, 0.8, "g");
+console.log("g")
+console.log(q.print());
+console.log(q.get_neighbourhood(0.1, 0.1));
 
-var n = 4096 * 2; // number of dots
-var dim = 2; // height/width of dot (square)
+let qt = new Quadtree(0, 0, w, h, qt_size);
+
+var allIceSet = new Set(); // array of dots
 
 var movingIce = new Array(n-1);
 var movingNum = n-1;
@@ -36,10 +185,53 @@ var stuckNum = 1;
 
 for (let i = 0; i < n - 1; i += 1) {
 	// randomly decide the rest
-	movingIce[i] = new Ice(Math.floor(Math.random() * w * 1 / 2) + w / 4,
-		Math.floor(Math.random() * h * 1 / 2) + h / 4, dim, dim);
+	let x = Math.floor(Math.random() * w * 1 / 2) + w / 4;
+	let y = Math.floor(Math.random() * h * 1 / 2) + h / 4;
+	movingIce[i] = new Ice(x, y, dim, dim);
 }
 
+function update_with_quadtree() {
+	qt = new Quadtree(0, 0, w, h, qt_size);
+
+	for (let i = 0; i < movingNum; i++) {
+		m = movingIce[i];
+		qt.add(m.x, m.y, i);
+	}
+
+	let new_stuck_indices = new Set();
+
+	for (let i = 0; i < stuckNum; i++) {
+		stuck = stuckIce[i];
+		nbhd = qt.get_neighbourhood(stuck.x, stuck.y).concat(qt.get_neighbourhood(stuck.x + stuck.width, stuck.y))
+		.concat(qt.get_neighbourhood(stuck.x, stuck.y + stuck.height)).concat(qt.get_neighbourhood(stuck.x, stuck.y + stuck.height));
+		for (let j = 0; j < nbhd.length; j++) {
+			m_index = nbhd[j];
+			nonStuck = movingIce[m_index];
+			if (stuck.x <= nonStuck.x + dim && stuck.x + dim >= nonStuck.x && stuck.y <= nonStuck.y + dim && stuck.y + dim >= nonStuck.y) { // and if they collide
+				new_stuck_indices.add(m_index);
+			}
+		}
+	}
+
+	for (let index of new_stuck_indices) {
+		stuckIce[stuckNum] = movingIce[index];
+		stuckNum += 1;
+		movingIce[index] = null;
+	};
+
+	let k = 0;
+	while (k < movingNum) {
+		if (movingIce[k] == null) {
+			movingIce[k] = movingIce[movingNum - 1];
+			movingNum--;
+		}
+		else {
+			movingIce[k].x += Math.floor(Math.random() * 3 - 1); // move it randomly
+			movingIce[k].y += Math.floor(Math.random() * 3 - 1);
+			k++;
+		}
+	}
+}
 
 function update() {
 	for (let i = 0; i < stuckNum; i++) {
@@ -62,18 +254,18 @@ function update() {
 		movingIce[i].y += Math.floor(Math.random() * 3 - 1);
 	}
 
-	// make stuck group move
-	// let randX = Math.floor(Math.random() * 3 - 1);
-	// let randY = Math.floor(Math.random() * 3 - 1);
-	// for (let i = 0; i < stuckNum; i++) {
-	// 	stuckIce[i].x += randX;
-	// 	stuckIce[i].y += randY;
-	// }
 }
 
 
 function render () {
 	context.clearRect(0, 0, w, h); // reset the canvas each time
+
+	context.strokeStyle = 'lightgray';
+	context.beginPath();
+	qt.draw();
+	context.lineWidth = 1;
+	context.stroke();
+
 	context.fillStyle = 'black';
 	for (let i = 0; i < movingNum; i++) {
 		context.fillRect(movingIce[i].x, movingIce[i].y, dim, dim);
@@ -86,9 +278,8 @@ function render () {
 
 
 function main () { // runs game
-	update();
+	update_with_quadtree();
 	render();
-
 	requestAnimationFrame(main);
 }
 
